61d6b37a4d479618faa27d297851b04f0d7cbb43
siosantiago <franzoni.jaramillo@gmail.com>
2023-06-27 07:05:49 +0000

    Finish toy app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,9 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  
+  #Allow Cloud9 connections.
+  config.hosts.clear
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
